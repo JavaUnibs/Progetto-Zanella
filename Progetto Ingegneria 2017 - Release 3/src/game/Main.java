eae0280bae8ad935ef34f266e46afe24051c4ac9
Fabio <Fabio@Fabio>
2017-05-10 23:12:56 +0200

    Corretti accenti release 3

--- Progetto Ingegneria 2017 - Release 3/src/game/Main.java
+++ Progetto Ingegneria 2017 - Release 3/src/game/Main.java
@@ -9,24 +9,24 @@ public class Main {
 	
 	static final String[] MENU_PRINCIPALE= {"Vai in una direzione", "Deposita una chiave", "Salva la sessione"}; 
 	static final String[] MENU_DIREZIONI = {"Avanti", "Indietro", "Sinistra", "Destra", "Sopra", "Sotto"};
-	static final String START_MSG="Benvenuto nel Labirinto di Minosse. L'obiettivo del gioco è farti strada\nattraverso le sue stanze e raggiungere la sala del tesoro, "
+	static final String START_MSG="Benvenuto nel Labirinto di Minosse. L'obiettivo del gioco Ã¨ farti strada\nattraverso le sue stanze e raggiungere la sala del tesoro, "
 			+ "ovvero la  ";
 	static final String START_MSG2= "Per riuscirci dovrai raccogliere chiavi di vari metalli e pesi\ne aprire dei passaggi tra i vari saloni. "
 			+ "\nBuona Fortuna!\n";
 	static final String MSG_NO_CAST = "Attenzione, ci sono problemi con il cast del file!";
 	static final String MSG_OK_FILE="File caricato correttamente";
 	static final String MSG_NO_FILE="File non caricato correttamente";
-	static final String FILE_EXISTS="Esiste giï¿½ un file con lo stesso nome";
+	static final String FILE_EXISTS="Esiste giÃ  un file con lo stesso nome";
 	static final String NO_GROUND= "Sei ai confini del labirinto, non puoi andare oltre.";
-	static final String CLOSED_PASSAGE="Questo passaggio è murato, non puoi andare oltre";
+	static final String CLOSED_PASSAGE="Questo passaggio Ã¨ murato, non puoi andare oltre";
 	static final String KEY_PRESENT="E' presente una chiave di: ";
 	static final String KEY_NEEDED="Questo passaggio necessita di una chiave di:";
 	static final String NO_KEY="Non possiedi la chiave giusta. Prova un'altra direzione";
 	static final String YES_KEY="Possiedi la chiave giusta, il passaggio si apre.";
-	static final String CURRENT_GROUND="La sala corrente è: ";
+	static final String CURRENT_GROUND="La sala corrente Ã¨: ";
 	static final String GET_KEY="Vuoi raccogliere la chiave?";
-	static final String WEIGHT="La chiave non puï¿½ essere raccolta perchï¿½ o il peso massimo o il numero massimo di chiavi trasportabili ecceduti";
-	static final String CANT_PUT_KEY1="Non puoi depositare chiavi, prima muoviti in un'altra direzione (Sei nel luogo iniziale, finale o c'è già una chiave)";
+	static final String WEIGHT="La chiave non puÃ² essere raccolta perchï¿½ o il peso massimo o il numero massimo di chiavi trasportabili ecceduti";
+	static final String CANT_PUT_KEY1="Non puoi depositare chiavi, prima muoviti in un'altra direzione (Sei nel luogo iniziale, finale o c'Ã¨ giÃ  una chiave)";
 	static final String CANT_PUT_KEY2="Non possiedi alcuna chiave";
 	static final String GOT_KEY="Hai raccolto la chiave";
 	static final String END="Sei finalmente arrivato alla sala del tesoro! Hai vinto il gioco!";
@@ -269,7 +269,7 @@ public class Main {
 						Token key=mondo.getKeys().get(scelta_chiavi-1);
 						luogo_corrente.setKey(key);
 						mondo.getKeys().remove(key);
-						System.out.println("La chiave scelta ï¿½ stata depositata");
+						System.out.println("La chiave scelta Ã¨ stata depositata");
 						mondo.setDepositata(true);
 
 					}
